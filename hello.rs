17b542b29988aadb02866947bd65777c9429d196
cjmartin <cjmartin@DESKTOP-COJA.home>
2024-07-31 06:24:29 -0700

    Added goals for program

--- hello.rs
+++ hello.rs
@@ -1,3 +1,7 @@
+// Goals for this program
+// Create a date time that holds grid coordinates, basically (X,Y)
+// Program takes a beginning coordinate and ending coordinate and caclulates heading, distance
+
 fn main() {
 	println!("Hello World!");
 }
